8d21bfa15d2e67723393f00f8e1b0ca0d8c41aee
MiguelCaAr <castroarredondo.hector@gmail.com>
2024-01-27 02:12:35 -0700

    Ahora solo se renderizan los cursos que creo el usuario con la sesion iniciada

--- src/components/Courses.vue
+++ src/components/Courses.vue
@@ -13,20 +13,24 @@
         </ul>
       </div>
       <div class="col-md-10 d-flex flex-row-reverse flex-wrap gap-3">
-        <div class="card" style="width: 18rem" v-for="(value, key) in storage" :key="key">
-          <div class="card-body">
-            <h5 class="card-title text-primary">{{ key }}</h5>
-            <p class="card-text">
-              {{ value.description }}
-            </p>
-            <p class="card-text text-secondary">Finalizacion del curso: {{ value.finishDate }}</p>
-            <div class="text-center">
-              <p>
-                <a href="#" @click.prevent="TogglePopUpDetails" @click="sendDetails(value)">Detalles</a>
-              </p>
+        <template v-for="(value, key) in storage" :key="key">
+          <template v-if="value.createdBy.includes($userStore.user)">
+            <div class="card" style="width: 18rem">
+              <div class="card-body">
+                <h5 class="card-title text-primary">{{ key }}</h5>
+                <p class="card-text">
+                  {{ value.description }}
+                </p>
+                <p class="card-text text-secondary">Finalizacion del curso: {{ value.finishDate }}</p>
+                <div class="text-center">
+                  <p>
+                    <a href="#" @click.prevent="TogglePopUpDetails" @click="sendDetails(value)">Detalles</a>
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
+          </template>
+        </template>
       </div>
     </section>
     <addCourse v-show="PopUp" v-on:TogglePopUp="TogglePopUp" v-on:GetCourses="GetCourses" />
@@ -73,6 +77,9 @@ const GetCourses = () => {
 const sendDetails = (courseDetails) => {
   $courseStore.updateDetails(courseDetails);
 };
+//------------Obtener usuario-----------//
+import { useUserStore } from "@/stores/userStore";
+const $userStore = useUserStore();
 
 onMounted(() => {
   GetCourses();
